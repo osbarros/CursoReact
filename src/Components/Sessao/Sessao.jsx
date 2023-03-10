0a7e01ccf3190c0b66f226a595861d5160ac7525
osbarros <ceobarros@outlook.com>
2023-03-10 18:40:20 -0300

    instalação styledComponents e estilização do componente Sessao

---
+++ src/Components/Sessao/Sessao.jsx
@@ -0,0 +1,30 @@
+import { Chegada, ContainerInfoUsuario, ContainerSessao, ContainerNomeStatus, TextoCargo, TextoHorario, TextoNome, TextoStatus } from "./Styles";
+import React from "react";
+
+function Sessao() {
+  return (
+    <ContainerSessao>
+      <ContainerInfoUsuario>
+        <ContainerNomeStatus>
+          <div>
+            <TextoNome>Oswaldo Barros</TextoNome>
+          </div>
+          <div>
+            <TextoStatus>"Totalmente operacional"</TextoStatus>
+          </div>
+        </ContainerNomeStatus>
+        <div>
+          <TextoCargo>Dev Líder</TextoCargo>
+        </div>
+      </ContainerInfoUsuario>
+      <Chegada>
+        <TextoHorario>21:34</TextoHorario>
+      </Chegada>
+      <Chegada>
+        <TextoHorario>01:34</TextoHorario>
+      </Chegada>
+    </ContainerSessao>
+  );
+}
+
+export default Sessao;
---
+++ src/Components/Sessao/Styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const ContainerSessao = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background: rgba(0, 0, 0, 0.81);
+  padding: 30px 3% 30px 3%;
+  width: 100%;
+  max-height: 85px;
+`
+
+export const Chegada = styled.div`
+  display: flex;
+  border-style: solid;
+  border-color: #FFE712;
+  border-width: 1px;
+  border-radius: 10px;
+  padding: 6px 15px 6px 15px; 
+  align-items: center;
+`
+
+export const TextoHorario = styled.text`
+  color: #FFE712;
+  font-size: 1 rem;
+`
+export const ContainerInfoUsuario = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+`
+export const ContainerNomeStatus = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 5%;
+`
+
+export const TextoNome = styled.text`
+  color: #F5F5F5;
+  font-size: 1 em;
+  line-height: 19px;
+`
+
+export const TextoStatus = styled.text`
+  color: #D2D2D2;
+  font-size: 1 em;
+  line-height: 19px;
+  font-weight: 300;
+`
+export const TextoCargo = styled.text`
+  color: #FFE712;
+  font-size: 1 rem;
+`
+
+
+
+
+
+
+
